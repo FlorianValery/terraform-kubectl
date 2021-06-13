bb83c8de96dfb9895f3e03c494dea2e31e576ab9
Florian Valery <florianvalery@helecloud.com>
2021-06-13 18:37:35 +0200

    Added image build script

---
+++ build_script/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eEuoa pipefail
+
+for terraform_version in ./*; do
+    for kubectl_version in "$terraform_version"/*; do
+        KUBE_V=$(awk -F  'kubectl'  '{print $2}'  <<<  $kubectl_version)
+        TERRA_V=$(awk -F  'kubectl'  '{print $1}'  <<<  $terraform_version | awk -F  'terraform'  '{print $2}' )
+        docker build -t floval/terraform-kubectl:${TERRA_V}-kubectl-${KUBE_V} $kubectl_version
+        docker push floval/terraform-kubectl:${TERRA_V}-kubectl-${KUBE_V}
+    done
+done 
