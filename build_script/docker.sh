44ec911db2c6c69f1c8ab53182cab06462fcc576
Florian Valery <florianvalery@helecloud.com>
2021-06-14 15:48:37 +0200

    Updated script to include all tags

--- build_script/docker.sh
+++ build_script/docker.sh
@@ -6,6 +6,8 @@ for terraform_version in ./*; do
         KUBE_V=$(awk -F  'kubectl'  '{print $2}'  <<<  $kubectl_version)
         TERRA_V=$(awk -F  'kubectl'  '{print $1}'  <<<  $terraform_version | awk -F  'terraform'  '{print $2}' )
         docker build -t floval/terraform-kubectl:${TERRA_V}-kubectl-${KUBE_V} $kubectl_version
-        docker push floval/terraform-kubectl:${TERRA_V}-kubectl-${KUBE_V}
+        
     done
 done 
+
+docker push --all-tags floval/terraform-kubectl
